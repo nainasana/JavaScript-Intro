7002d177b8b66516c4475c2f0e5a54e60aa73fcd
NainaSana <104092313+nainasana@users.noreply.github.com>
2022-07-28 16:55:53 +0530

    Delete metaprog.js

--- metaprog.js
+++
@@ -1,9 +0,0 @@
-let handler = {
-    get(target, name) {
-        return name in target ? target[name] : 55
-    }
-}
-
-let p = new Proxy({}, handler)
-p.a = 51
-console.log(p.a +", "+ p.b) // 51, 55
