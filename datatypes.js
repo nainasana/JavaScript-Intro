11f9478bb42eeb39cf0cf1e88cb40b54c8950a43
NainaSana <104092313+nainasana@users.noreply.github.com>
2022-07-30 11:39:58 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,6 @@
+let number = 100;
+let name = "Talentio";
+let flag = true;
+console.log("100 is a", typeof (number), "data type.");
+console.log("Talentio is a", typeof (name), "data type.");
+console.log("true is a", typeof (flag), "data type.");
